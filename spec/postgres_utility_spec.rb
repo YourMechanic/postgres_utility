3ab6f897efd655c5b88fb8963ba7087314491ced
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2021-09-27 19:19:22 +0530

    added test rspec test cases

--- spec/postgres_utility_spec.rb
+++ spec/postgres_utility_spec.rb
@@ -1,11 +1,71 @@
 # frozen_string_literal: true
 
 RSpec.describe PostgresUtility do
+  # before(:each) do
+  #   ActiveRecord::Base.connection.execute %{
+  #     TRUNCATE TABLE test_models;
+  #     SELECT setval('test_models_id_seq', 1, false);
+  #   }
+  #   TestModel.create(data: 'test data 1')
+  # end
+
   it "has a version number" do
     expect(PostgresUtility::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe ".db_name" do
+    it "returns the db name" do
+      expect(PostgresUtility.db_name).to eq("pg_utility_db")
+    end
+  end
+
+  describe ".rails_connection" do
+    it "returns the db connection" do
+      expect(PostgresUtility.rails_connection).to be
+    end
+  end
+
+  describe ".db_adapter_name" do
+    it "returns the db adapter name" do
+      expect(PostgresUtility.db_adapter_name).to eq("postgresql")
+    end
+  end
+
+  describe ".postgresql?" do
+    it "returns true if adapter is postgresql" do
+      expect(PostgresUtility.postgresql?).to be_truthy
+    end
+  end
+
+  describe ".db_version" do
+    it "gives the database version" do
+      expect(PostgresUtility.db_version).to be
+    end
+  end
+
+  describe ".db_size" do
+    it "gives the database size" do
+      expect(PostgresUtility.db_size).to be
+    end
+  end
+
+  describe ".create_database" do
+    it "returns false if db already exists" do
+      expect(PostgresUtility.create_database).to eq(false)
+    end
+  end
+
+  describe ".recreate_database" do
+    xit "false if db already exists" do
+      expect(PostgresUtility.recreate_database).to eq(false)
+    end
+  end
+
+  describe ".copy_table_query" do
+    let!(:samp_record) { TestModel.create(data: "good copy text") }
+
+    it "returns query that copies records from source to destination table" do
+      expect(PostgresUtility.copy_table_query(TestModel, DestinationTestModel)).to be
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@
 
 require "postgres_utility"
 
-require 'fixtures/test_model'
+require "fixtures/test_model"
+require "fixtures/destination_test_model"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -18,32 +19,33 @@ RSpec.configure do |config|
   config.before(:suite) do
     # we create a test database if it does not exist
     # I do not use database users or password for the tests, using ident authentication instead
-    begin
-      ActiveRecord::Base.establish_connection(
-        :adapter  => "postgresql",
-        :host     => "localhost",
-        :username => "postgres",
-        :password => "postgres",
-        :port     => 5432,
-        :database => "pg_utility_db"
-      )
-      ActiveRecord::Base.connection.execute %{
+
+    ActiveRecord::Base.establish_connection(
+      adapter: "postgresql",
+      host: "localhost",
+      username: "postgres",
+      password: "postgres",
+      port: 5432,
+      database: "pg_utility_db"
+    )
+    ActiveRecord::Base.connection.execute %{
         SET client_min_messages TO warning;
         DROP TABLE IF EXISTS test_models;
+        DROP TABLE IF EXISTS destination_test_models;
         CREATE TABLE test_models (id serial PRIMARY KEY, data text);
+        CREATE TABLE destination_test_models (id serial PRIMARY KEY, data text);
       }
-    rescue Exception => e
-      puts "Exception: #{e}"
-      ActiveRecord::Base.establish_connection(
-        :adapter  => "postgresql",
-        :host     => "localhost",
-        :username => "postgres",
-        :password => "postgres",
-        :port     => 5432,
-        :database => "postgres"
-      )
-      ActiveRecord::Base.connection.execute "CREATE DATABASE pg_utility_db"
-      retry
-    end
+  rescue StandardError => e
+    puts "Exception: #{e}"
+    ActiveRecord::Base.establish_connection(
+      adapter: "postgresql",
+      host: "localhost",
+      username: "postgres",
+      password: "postgres",
+      port: 5432,
+      database: "postgres"
+    )
+    ActiveRecord::Base.connection.execute "CREATE DATABASE pg_utility_db"
+    retry
   end
 end
