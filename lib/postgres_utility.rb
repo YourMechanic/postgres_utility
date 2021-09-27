b9e84384c1cd74616e82b1cfa6423f68e9820eb4
sachinsaxena1996 <sachinsaxena1996@gmail.com>
2021-09-27 11:31:48 +0530

    initial gem repository files

---
+++ lib/postgres_utility.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "postgres_utility/version"
+
+module PostgresUtility
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/postgres_utility_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe PostgresUtility do
+  it "has a version number" do
+    expect(PostgresUtility::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
